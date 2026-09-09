0f56015e6464a0485b3045786d9a0736b3783f17
juhkast <juhkast@laurea.fi>
2026-09-09 19:33:38 +0300

    js sample program

---
+++ lecture-example/03-js/script.js
@@ -0,0 +1,28 @@
+// 1: Print text to the browser console is string
+
+console.log("Hello world!");
+
+// variables
+
+const userName = "Juho";
+const favoriteAnimal = "blue elephant";
+
+// print text values in variables to the console
+
+console.log("My name is " + userName);
+console.log("My favorite animal is " + favoriteAnimal);
+
+// STEP 3: User interactions
+
+alert("Welcome to the Full Stack Programming course!");
+
+const visitorName = prompt("What is your name?");
+
+console.log(visitorName);
+
+// create a personalized greeting message
+console.log("Hello " + visitorName + "! Welcome to the Full Stack Programming course!");
+
+const visitorFavoriteAnimal = prompt("What is your favorite animal?");
+console.log("Hello " + visitorName + "! Your favorite animal is the " + visitorFavoriteAnimal + ".");
+
